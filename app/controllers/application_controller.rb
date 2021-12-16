36e4bb4edc7354043f48969ef2140dc3be4a941c
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-12-16 18:37:16 -0300

    Criando user padrão e corrigindo rota

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+    before_action :createUser
     acts_as_token_authentication_handler_for User
     before_action :require_authentication!
  
@@ -6,5 +7,9 @@ class ApplicationController < ActionController::API
     
     def require_authentication!
         throw(:warden, scope: :user) unless current_user.presence
+    end
+
+    def createUser
+        User.create email: 'teste-api@gmail.com', password: '123456'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :tags
   get 'quotes/:tag', to: 'tags#quotes'
   get 'frases', to: 'tags#frases'
-  get 'buscar_frase_author/author=:author', to: 'tags#buscar_frase_author'
+  get 'buscar_frase_author/:author', to: 'tags#buscar_frase_author'
   devise_for :users
 end
